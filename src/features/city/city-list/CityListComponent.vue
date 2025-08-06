39ee67f94a1c2537baddcce97a73f4ba0a17804c
rane <sudeep.rane@smartstream-stp.com>
2025-08-07 00:03:04 +0530

    feat(city): implement basic search on citylist

--- src/features/city/city-list/CityListComponent.vue
+++ src/features/city/city-list/CityListComponent.vue
@@ -4,9 +4,18 @@
       <h2>Weather</h2>
       <div @click="goToProfile()" class="city-list_profile"></div>
     </div>
+    <div class="city-list_search">
+      <input
+        id="input-city-filter"
+        v-model="searchString"
+        type="text"
+        class="city-list_search__input"
+        placeholder="Search your City"
+      />
+    </div>
     <div class="city-list_body">
       <CityCard
-        v-for="city in cityStore.cities"
+        v-for="city in filteredCities"
         :key="city.name"
         :city="city"
         @click="selectCity(city.name)"
@@ -16,15 +25,21 @@
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 import { useRouter } from 'vue-router';
 import CityCard from '../city-card/CityCard.vue';
 import { useCityStore } from '../stores/cityStore';
 
 const selectedCity = ref<string | null>(null);
-
 const cityStore = useCityStore();
 const router = useRouter();
+const searchString = ref<string>('');
+
+const filteredCities = computed(() => {
+  const query = searchString.value.toLowerCase().trim();
+  if (!query) return cityStore.cities;
+  return cityStore.cities.filter((city) => city.name.toLowerCase().includes(query));
+});
 
 const selectCity = (name: string) => {
   selectedCity.value = name;
